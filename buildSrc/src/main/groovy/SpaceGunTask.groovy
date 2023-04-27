85b8997bd1895aa62ef9772486ce3b98a03d9a20
Ашутов Алексей <ashutov@iteco-inno.ru>
2023-04-27 14:31:01 +0300

    add task

--- buildSrc/src/main/groovy/SpaceGunTask.groovy
+++ buildSrc/src/main/groovy/SpaceGunTask.groovy
@@ -5,6 +5,13 @@ class SpaceGunTask extends DefaultTask{
 
     @TaskAction
     def fire() {
-        println(" Somthing fiiiiirere!!!!")
+        def spacefile= project.file('spaceoutput/spacegun.txt')
+        spacefile.parentFile.mkdirs()
+        spacefile.write(" Somthing fiiiiirere!!!!")
+
+        println("-----------------")
+        println("Look in to the file friend: "+spacefile.name)
+        println("-----------------")
+
     }
 }
