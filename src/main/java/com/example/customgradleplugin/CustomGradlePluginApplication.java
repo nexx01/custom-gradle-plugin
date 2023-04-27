494cfc41607ecc460a9a2d207c5b21229ff32657
Ашутов Алексей <ashutov@iteco-inno.ru>
2023-04-27 13:16:49 +0300

    Initial commit

---
+++ src/main/java/com/example/customgradleplugin/CustomGradlePluginApplication.java
@@ -0,0 +1,13 @@
+package com.example.customgradleplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomGradlePluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomGradlePluginApplication.class, args);
+    }
+
+}
